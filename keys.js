d2d70f81aef8712ec5b23f5c682c49d9ebf50d3e
SaulValo <SaulValo@MacBook-Pro-2.local>
2017-04-07 20:31:23 -0700

    added changes to keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'ZLN8jSDnkujNr73iBymdLjVoS',
-  consumer_secret: 'x4lfAzMkopfhZ6tEq70ouaajQeXmA98DdFMoVqw0maA8gPTvhw',
-  access_token_key: '40433507-c04GcNsPbw9ILl0PAEgUuS0PHOkuaKeJByg6YT4Lq',
-  access_token_secret: 'jJiUVZGIqNcYMGKoWPjS1o5SzISEX3rtYKFyVGOtTcAGi',
+  consumer_key: 'BDrWGp9QDIb5Gt5k3aXGFWPQg',
+  consumer_secret: 'OE0XjAFy0NU5KijxfWIRaSig4yIPziNrrwC7ru9Woi6Mrf4zTh',
+  access_token_key: '40433507-IHDgOgsK1eizhNfSGtN0gsjWz3dsFHHahEqI3hham',
+  access_token_secret: 'HN1Kr9Si3RoRP9Cgfyl2yiI8JhhaaG7bxxk6Yzp7iwQf3',
 }
