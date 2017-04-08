b84fac6ec335b45177c31d682c935e7da4f53ccd
Saul Arevalo <saul.arevalo.8@gmail.com>
2017-04-07 20:20:03 -0700

    Delete key.js

--- key.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'ZLN8jSDnkujNr73iBymdLjVoS',
-  consumer_secret: 'x4lfAzMkopfhZ6tEq70ouaajQeXmA98DdFMoVqw0maA8gPTvhw',
-  access_token_key: '40433507-c04GcNsPbw9ILl0PAEgUuS0PHOkuaKeJByg6YT4Lq',
-  access_token_secret: 'jJiUVZGIqNcYMGKoWPjS1o5SzISEX3rtYKFyVGOtTcAGi',
-}
